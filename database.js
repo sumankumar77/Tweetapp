d880afe29b6c465082bf2fc927e6aaea4fa37c3a
sumankumar77 <kallurusuman99@gmail.com>
2024-10-26 18:17:25 -0400

    database file

---
+++ database.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+
+class Database{
+
+    
+    constructor(){
+        this.connect();
+    }
+
+
+
+
+
+    connect(){
+        mongoose.connect("mongodb+srv://admin:dbuserpassword@twitterclonecluster.jpzgg.mongodb.net/?retryWrites=true&w=majority&appName=TwitterCloneCluster")
+        .then(() => {
+         console.log("database connection successful");
+    })
+        .catch((err) => {
+        console.log("database connection error " + err);
+    })
+    }
+
+}
+
+module.exports = new Database();
